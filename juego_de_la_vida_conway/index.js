97be6f3c626c3434b391d68676b691f625952f05
Alexis <alexis.moraga.gallardo@gmail.com>
2024-08-07 17:20:30 -0400

    - Se refactoriza el codigo para contar vecinos - se optimiza la creacion de la matriz inicial reduciendo el numero de elementos - se añade una grilla - Se actualiza el color del tablero - se añade la funcionalidad de iniciar un tablero en blanco y establecer las celulas mediante clicks - se añade el conteo de problacion - Se añade el conteo de generaciones

--- juego_de_la_vida_conway/index.js
+++ juego_de_la_vida_conway/index.js
@@ -1,46 +1,78 @@
 /**Controls */
-let state = 0;
-let interval = null;
-let gen = 0;
+let state = 0
+let interval = null
+let gen = 0
+const cellSize = 15
+const cellColor = 'white'
+const backgroundColor = 'black'
+var board = []
 
 //Canvas
 const canvas = document.getElementById('gameOfLifeCanvas')
 const ctx = canvas.getContext('2d')
-ctx.canvas.width = window.innerWidth -1; //set width
-ctx.canvas.height = window.innerHeight-1; // set height
+ctx.canvas.width = window.innerWidth -1 //set width
+ctx.canvas.height = window.innerHeight-1 // set height
+ctx.strokeStyle = 'blue'
+ctx.lineWidth = 0.4
 
-const cellSize = 5;
-const cellColor = 'black';
-ctx.fillStyle = cellColor;
-
-var mapp = [];
+const lengthY = Math.round(canvas.height / cellSize)
+const lengthX = Math.round(canvas.width / cellSize)
 
 //Establece el contenido del array map con los valores que iniciaran el juego
-function initialState() {
-    mapp = Array.from({length: canvas.height}, () => {
-        return Array.from({length: canvas.width}, () => Math.random() < 0.6 ? 0 : 1)
+function initialState(initialBoard) {
+    board = Array.from({length: lengthY}, () => {
+        return Array.from({length: lengthX}, () => {
+            if(initialBoard){
+                return Math.random() < 0.85 ? 0 : 1
+            }else{
+                return 0   
+            }
+            }
+        )
     })
-    console.log(mapp)
+    console.log(board)
 }
 
 //Funcion encargada de pintar las celuldas en cada iteracion
 function renderGame(map){
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // Limpiar el canvas antes de dibujar
+    
+    ctx.fillStyle = cellColor
     map.forEach((row, y) =>{
         row.forEach((cell, x) => {
-            cell && ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
+            cell && ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize)
         })
     })
+
+    // Dibujar las líneas horizontales
+    for (let y = 0; y <= map.length; y++) {
+        ctx.beginPath();
+        ctx.moveTo(0, y * cellSize);
+        ctx.lineTo(canvas.width, y * cellSize);
+        ctx.stroke();
+    }
+
+    // // Dibujar las líneas verticales
+    for (let x = 0; x <= map[0].length; x++) {
+        ctx.beginPath()
+        ctx.moveTo(x * cellSize, 0);
+        ctx.lineTo(x * cellSize, canvas.height)
+        ctx.stroke()
+    }
+    ctx.stroke()
+
+    getPoblation()
+    getGen()
 }
 
 //Funcion ecargada de revisar, validar y crear el nuevo arreglo con los valores de las celulas
 function validate(){
-    const newMap = structuredClone(mapp);
+    const newMap = structuredClone(board);
 
-    mapp.forEach((row, y) =>{
+    board.forEach((row, y) =>{
         row.forEach((cell, x) => {
-
-            let neighbors = getNeightbors(x, y); 
-
+            let neighbors = getNeightbors(y, x); 
             if (cell && (neighbors == 2 || neighbors == 3)){
                 newMap[y][x] = 1;
             }else if(!cell && neighbors == 3){
@@ -48,79 +80,94 @@ function validate(){
             }else{
                 newMap[y][x] = 0;
             }
-
         })
     })
-    mapp = newMap;
+    board = newMap;
 }
 
 //Funcion encargada de contar los vecinos vivos de una celula
-function getNeightbors(x, y){
-    let neighbors = 0;
+function getNeightbors(y, x){
+    let neighbors = 0
 
-    if(y > 0){
-        if(mapp[y-1][x]) neighbors++;
-        if(mapp[y-1][x-1]) neighbors++;
-        if(mapp[y-1][x+1]) neighbors++;
-    }
+    //01 => arriba a la izquierda
+    if(y > 0 && x > 0 && board[y-1][x-1]) neighbors++
 
-    if(x > 0){
-        if(mapp[y][x-1]) neighbors++;
-    }
-    
-    if(y < mapp.length -1){
-        if(mapp[y+1][x-1]) neighbors++;
-        if(mapp[y+1][x]) neighbors++;
-        if(mapp[y+1][x+1]) neighbors++;
-    }
+    //02 => arriba
+    if(y > 0 && board[y-1][x]) neighbors++
 
-    if(x < mapp.length -1){
-        if(mapp[y][x+1]) neighbors++;
-    }
+    //03 => arriba a la derecha
+    if(y > 0 && x < lengthX -1 && board[y-1][x+1]) neighbors++
 
-    return neighbors;
-}
+    //04 => izquierda
+    if(x > 0 && board[y][x-1]) neighbors++
 
-//Limpia el canvas
-function clearCanvas(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //05 => derecha
+    if(x < lengthX -1 && board[y][x+1]) neighbors++
+
+    //06 => abajo a la izquierda
+    if(y < lengthY -1 && x > 0 && board[y+1][x-1]) neighbors++
+
+    //07 => abajo
+    if(y < lengthY -1 && board[y+1][x]) neighbors++
+
+    //08 => abajo a la derecha
+    if(y < lengthY -1 && x < lengthX -1 && board[y+1][x+1]) neighbors++
+
+    return neighbors
 }
 
 //Reinicia el juego
-function clearBoard(){
-    initialState()
-    clearInterval(interval);
-    state && changeState();
-    clearCanvas();
-    gen = 0;
-    document.getElementById('gen').innerHTML = `Gen: 0`;
-    renderGame(mapp);
+function clearBoard(initialBoard){
+    initialState(initialBoard)
+    clearInterval(interval)
+    state && changeState()
+    gen = 0
+    renderGame(board)
 }
 
 //Realiza el cambio de estado del juego
 const changeState = () => {
-    state = state ? 0 : 1;
-    document.getElementById('playPause').innerHTML = state ? 'Pause' : 'Play';
+    state = state ? 0 : 1
+    document.getElementById('playPause').innerHTML = state ? 'Pause' : 'Play'
 }
 
+const getPoblation = () => {
+    let poblation = board.reduce((sum, row) =>{
+        return sum + row.reduce((ac, num) => ac + num, 0);
+    },0)
+    document.getElementById('poblation').innerHTML = `Cells: ${poblation}`
+}
+
+//renderiza el numero de generaciones
+const getGen = () =>{
+    document.getElementById('gen').innerHTML = `Gen: ${gen++}`
+}
+    
 //Actualiza el juego en cada frame
 function update(){
-    clearCanvas();
-    validate();
-    renderGame(mapp);
-    document.getElementById('gen').innerHTML = `Gen: ${gen++}`;
+    validate()
+    renderGame(board)
 }
 
 //inicia o pausa el juego
 function playPause(){
-    changeState();
+    changeState()
     if(state){
-        interval = setInterval(update, 150);
+        interval = setInterval(update, 50)
     }else{
-        clearInterval(interval);
+        clearInterval(interval)
     }
 }
 
+//añade celulas de forma manual en cada pixel
+canvas.addEventListener('click', (e) => {
+    const x = Math.floor(e.offsetX / cellSize)
+    const y = Math.floor(e.offsetY / cellSize)
+    board[y][x] = board[y][x] ? 0 : 1
+    renderGame(board)
+})
 
-initialState();//establece las celulas iniciales
-renderGame(mapp);//renderiza las celulas iniciales
+
+initialState(1)//establece las celulas iniciales
+renderGame(board)//renderiza las celulas iniciales
+
